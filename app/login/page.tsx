4ae499aa7e3612130f6efbef2d4bb92568e5a024
markci-eng <markci@stpeter.com.ph>
2026-02-10 16:51:12 +0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import {LoginPage} from "osp-chakra-reusable-components";
-import { LoginPage } from "./LoginPageComponent";
+import { LoginPage } from "@splpi/signin-signup";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -17,19 +17,26 @@ interface ApiResponse {
 export default function Login(){
     const router = useRouter();
     const [users, setUsers] = useState<ApiResponse | null>(null);
-
+    const [rawId, setRawId] = useState<Uint8Array | null>(null);
     useEffect(() => {
         const fetchData = async () => {
-        const response = await fetch("/api/users");
-        if (!response.ok) {
-            throw new Error("Failed to fetch data");
-        }
+            const response = await fetch("/api/users");
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
 
-        const result: ApiResponse = await response.json();
-        setUsers(result);
+            const result: ApiResponse = await response.json();
+            setUsers(result);
+
+            const data = await navigator.credentials.get({
+            publicKey:{
+                challenge: new Uint8Array([0, 1, 2, 3, 4, 5, 6]),
+                allowCredentials: [{type: "public-key", id: rawId ?? new Uint8Array([0])}]
+            }
+        })
         };
 
-        fetchData(); // Call the function on component mount
+        fetchData();
     }, []);
 
     const handleSignUp = async (email: string, password: string, firstname: string, middlename: string, lastname: string, contactnumber: string) => {
@@ -76,6 +83,22 @@ export default function Login(){
     }
 
     const handleSignIn = async (email: string, password: string) => {
+        const data = await navigator.credentials.create({ publicKey: {
+                challenge: new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+                rp: { name: "OSP eStore" },
+                user: {
+                    id: new Uint8Array(16),
+                    name: email,
+                    displayName: email,
+                },
+                pubKeyCredParams: [
+                    { type: "public-key", alg: -7 },
+                    { type: "public-key", alg: -8 },
+                    { type: "public-key", alg: -257 },
+                ]
+            } })
+            setRawId(new Uint8Array((data as PublicKeyCredential).rawId));
+            console.log(data)
         //Validate email and password
         try {
             const filteredItems = Array.isArray(users) && users.length > 0
@@ -91,7 +114,6 @@ export default function Login(){
                 return;
             }
             localStorage.setItem("user_data", JSON.stringify(filteredItems[0]));
-            console.log('User signed in:', filteredItems[0]);
             router.push('/plan-management');
         } catch (error) {
             console.error('Error during sign-in:', error);
